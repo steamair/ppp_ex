b2cfd6c3048a6a91b4001c2039c80471a8869f48
steamair <45411785+steamair@users.noreply.github.com>
2023-01-22 21:16:54 -0500

    practice

---
+++ ch_21/drills/practice.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //add code below this line
+
+
+string names[ ][5] = { {"Alan", "Bob", "Carol", "David", "Ellen"},
+                       {"Fred", "Grace", "Henry", "Ian", "Jen"},
+                       {"Kelly", "Liam", "Mary", "Nancy", "Owen"} };
+    
+cout << names[1][2] << endl;
+
+  //add code above this line
+  
+  return 0;
+  
+}
